eff0ab78f5c573801caf84424832d4d86e70bf2d
Noah <121849234+NoahNonOff@users.noreply.github.com>
2023-12-13 12:13:57 +0100

    fixing bug in check_parse

--- srcs/parsing/check_parse.c
+++ srcs/parsing/check_parse.c
@@ -1,21 +1,32 @@
 #include "minishell.h"
 
+static bool	nothing_after(const char *prompt, int code)
+{
+	int	i;
+
+	i = 0;
+	while (prompt[i] && m_isWhitespace(prompt[i]))
+		i++;
+	if (code)
+	{
+		if (!prompt[i] || prompt[i] == '|'
+			|| prompt[i] == '<' || prompt[i] == '>')
+			return (true);
+	}
+	else
+		if (!prompt[i] || prompt[i] == '|')
+			return (true);
+	return (false);
+}
+
 static bool	check_pipe(const char *prompt, int i)
 {
 	int	len;
 
-	len = i + 1;
-	while ((!m_isWhitespace(prompt[len]) && prompt[len] != '|') && prompt[len])
-		len++;
-	if (len == m_strlen(prompt) || prompt[len] == '|')
-	{
-		m_putstr("bash: parse error near `|'\n", 2);
-		return (false);
-	}
-	i--;
-	while ((!m_isWhitespace(prompt[i]) && prompt[i] != '|') && i > -1)
-		i--;
-	if (i == -1 || prompt[i] == '|')
+	len = i - 1;
+	while (len > -1 && m_isWhitespace(prompt[len]))
+		len--;
+	if (len == -1 || prompt[len] == '|' || nothing_after(&prompt[i + 1], 0))
 	{
 		m_putstr("bash: parse error near `|'\n", 2);
 		return (false);
@@ -28,7 +39,8 @@ static bool	check_red(const char *prompt, int *i)
 	bool	status;
 
 	status = true;
-	if (!prompt[(*i) + 1] || prompt[(*i) + 1] == '|')
+	if (!prompt[(*i) + 1] || prompt[(*i) + 1] == '|'
+		|| nothing_after(&prompt[(*i) + 1], 1))
 		status = false;
 	else if (prompt[(*i) + 1] == '<' || prompt[(*i) + 1] == '>')
 	{
@@ -36,7 +48,8 @@ static bool	check_red(const char *prompt, int *i)
 		if (prompt[(*i)] != prompt[(*i) - 1])
 			status = false;
 		else if (!prompt[(*i) + 1] || prompt[(*i) + 1] == '|'
-			|| prompt[(*i) + 1] == '>' || prompt[(*i) + 1] == '<')
+			|| prompt[(*i) + 1] == '>' || prompt[(*i) + 1] == '<'
+			|| nothing_after(&prompt[(*i) + 1], 1))
 			status = false;
 	}
 	if (!status)
